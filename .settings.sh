9c2641e622112713fef7da5e7022b11e08e6cdca
Joe Wang <joe@lacontech.com>
2015-01-28 21:41:54 +0800

    adding stuffs into garage

---
+++ .settings.sh
@@ -0,0 +1,63 @@
+# to use this file, put the following in ~/.bashrc, for mac ~/.bash_profile
+# source ~/.settings.sh
+
+##### alias settings  ######
+# general
+if [[ ${HOSTNAME:0:10} != "MBA-Wispur" ]]; then # mac pain...
+	alias ls='ls -G'
+fi
+alias l='ls -CF'
+alias la='ls -A'
+alias ll='ls -alF'
+alias lll='ls -l'
+alias vi='vim'
+alias g='egrep -i --color'
+alias g2='egrep -niH --color'
+alias psg='ps aux | grep -i --color'
+alias gall='find . -type f -print0 | xargs -0 egrep -niH --color'
+alias glog='find ~/logs -type f -print0 | xargs -0 egrep -niH --color'
+alias untar='tar xvfz'
+alias tarit='tar cvfz'
+alias alarm='echo -e "\a"'
+
+# misc
+alias vi='vim'
+alias loadsettings='source ~/.settings.sh'
+alias psweb='ps -u urworks -o rss,etime,pid,command'
+alias gitl='git log --all --graph --decorate'
+alias gits='git status'
+alias gita='git add'
+alias gitc='git commit -m'
+alias gitd='git diff'
+alias viconf='vi wp-config.php'
+alias apache='sudo service apache2'
+alias nginx='sudo service nginx'
+alias resapa='apache restart'
+alias lamp='sudo /opt/lampp/lampp'
+alias ressam='sudo restart smbd && sudo restart nmbd'
+alias pm='python manage.py'
+alias pms='pm syncdb'
+alias pmr='pm runserver'
+alias bpm='bpython manage.py'
+alias fandroid='adb forward tcp:9999 localabstract:chrome_devtools_remote' # forwards android chrome debugging
+alias pinghinet='ping 168.95.1.1'
+alias fixpermissions='sudo chown -R www-data:www-data .; find -type f |xargs sudo chmod 664; find -type d |xargs sudo chmod 775'
+alias sub='open /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+
+# MacBook Air spcific
+if [[ ${HOSTNAME:0:10} = "MBA-Wispur" ]]; then
+	export CLICOLOR=1
+	export LSCOLORS=ExFxCxDxBxegedabagacad
+	export LS_COLORS=ExFxCxDxBxegedabagacad
+	alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
+	export PYTHON_PATH='/usr/local/lib/python2.7/site-packages/django:$PYTHON_PATH'
+	export PYTHONPATH='/usr/local/lib/python2.7/site-packages:$PYTHONPATH'
+	# paths
+	alias cdwo='cd /Users/joe/Workspace'
+	alias checkport='lsof -i :8080'
+	alias diff='/Applications/DiffMerge.app/Contents/MacOS/DiffMerge'
+	ssh-add ~/.ssh/joeOSX
+fi
+
+# set prompt colors
+export PS1="\[\033[36m\]\u\[\033[37m\]@\[\033[36m\]\h\[\[\033[37m\]:\[\033[35m\]\w\[\033[37m\]\$ \[\033[m\]"
---
+++ .vimrc
@@ -0,0 +1,32 @@
+set nocompatible
+syntax on
+set t_Co=257				"256 colors
+set number					"line number
+set autoindent
+set tabstop=4
+set softtabstop=4
+set listchars=eol:$,tab:>-,trail:~,extends:>,precedes:<
+autocmd Filetype python setlocal expandtab      "expand tab only for python
+set hlsearch				"highlight search
+hi Search ctermbg=88			"red
+set ignorecase  "when searching for text
+hi comment cterm=bold ctermfg=white
+set cursorline
+hi clear cursorline
+hi cursorline ctermbg=darkgrey
+hi clear visual
+hi visual ctermbg=darkgrey
+hi statement ctermfg=magenta
+hi preproc ctermfg=magenta
+hi string ctermfg=11 "yellow
+hi constant ctermfg=11	"yellow
+hi identifier ctermfg=13 "purple
+hi function ctermfg=14 "cyan
+hi MatchParen ctermfg=white ctermbg=red
+hi Type ctermfg=lightblue
+hi special ctermfg=lightblue
+hi LineNr ctermfg=lightgray
+hi Title ctermfg=10
+"hi operator ctermfg=white
+"match operator /[-+=[](){}]/
+

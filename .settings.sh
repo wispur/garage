fdd565a7032474763c7bb2249b562593fb711247
Joe Wang <joewang00@gmail.com>
2015-02-13 12:42:03 +0800

    clean up stuffs

--- .settings.sh
+++ .settings.sh
@@ -3,7 +3,7 @@
 
 ##### alias settings  ######
 # general
-if [[ ${HOSTNAME:0:10} != "MBA-Wispur" ]]; then # mac pain...
+if [[ ${HOSTNAME:0:9} != "wispurAir" ]]; then # mac pain...
 	alias ls='ls -G --color'
 fi
 alias l='ls -CF'
@@ -31,7 +31,7 @@ alias gitc='git commit -m'
 alias gitd='git diff'
 alias gitdc='gitd --cached'
 alias viconf='vi wp-config.php'
-if [[ ${HOSTNAME:0:10} != "MBA-Wispur" ]]; then # mac pain...
+if [[ ${HOSTNAME:0:9} != "wispurAir" ]]; then # mac pain...
   alias apache='sudo service apache2'
   alias nginx='sudo service nginx'
 fi
@@ -46,32 +46,37 @@ alias fandroid='adb forward tcp:9999 localabstract:chrome_devtools_remote' # for
 alias pinghinet='ping 168.95.1.1'
 alias fixpermissions='sudo chown -R www-data:www-data .; find -type f |xargs sudo chmod 664; find -type d |xargs sudo chmod 775'
 alias sub='open /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
-
-# MacBook Air spcific
-if [[ ${HOSTNAME:0:10} = "MBA-Wispur" ]]; then
-	export CLICOLOR=1
-	export LSCOLORS=ExFxCxDxBxegedabagacad
-	export LS_COLORS=ExFxCxDxBxegedabagacad
-	alias subl='/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl'
-	export PYTHON_PATH='/usr/local/lib/python2.7/site-packages/django:$PYTHON_PATH'
-	export PYTHONPATH='/usr/local/lib/python2.7/site-packages:$PYTHONPATH'
-	# paths
-	alias cdwo='cd /Users/joe/Workspace/ibeengo'
-	alias checkport='lsof -i :8080'
-	alias diff='/Applications/DiffMerge.app/Contents/MacOS/DiffMerge'
-	# terminal title
-	function title {
-	  printf "\033]0;%s\007" "$1"
-	}
-	# postgres
-	alias pg='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log'
-fi
+function title {
+	printf "\033]0;%s\007" "$1"
+}
 
 # set prompt colors
 export PS1="\[\033[36m\]\u\[\033[37m\]@\[\033[36m\]\h\[\[\033[37m\]:\[\033[35m\]\w\[\033[37m\]\$ \[\033[m\]"
 
 # ssh
-alias sshtest='ssh deploy@test.ibeengo.com'
-alias sshdeploy='ssh deploy@ibeengo.com'
+alias sshnew='title deploy@new; ssh deploy@106.185.25.161' # new server
+alias sshold='title deploy@old; ssh deploy@ibeengo.com' # old server
 
+# MacBook Air spcific
+if [[ ${HOSTNAME:0:9} = "wispurAir" ]]; then
+	export CLICOLOR=1
+	export LSCOLORS=ExFxCxDxBxegedabagacad
+	export LS_COLORS=ExFxCxDxBxegedabagacad
+	# paths
+	alias cdwo='cd /Users/joe/Workspace/ibeengo'
 
+	alias checkport='lsof -i :8080'
+	alias diff='/Applications/DiffMerge.app/Contents/MacOS/DiffMerge'
+	# terminal title
+	title local
+	# postgres
+	alias pg='pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log'
+
+	alias flushdns='sudo discoveryutil udnsflushcaches'
+fi
+
+# ibeengo ubuntu
+if [[ ${HOSTNAME:0:7} = "ibeengo" ]];then
+	alias nginx='sudo service nginx'$
+	alias spsql='sudo -u postgres psql'$
+fi
